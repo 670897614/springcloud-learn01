8d78d833106b08e0d808cc17913c974782a3b60c
670897614 <JinLin_LL@163.com>
2020-11-24 18:04:06 +0800

    eureka注册中心

--- springcloud-provider-student-8001/src/main/java/com/ljerii/springcloud/controller/StudentController.java
+++ springcloud-provider-student-8001/src/main/java/com/ljerii/springcloud/controller/StudentController.java
@@ -3,6 +3,8 @@ package com.ljerii.springcloud.controller;
 import com.ljerii.springcloud.entity.Student;
 import com.ljerii.springcloud.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,10 @@ public class StudentController {
 
     @Autowired
     private StudentService studentService;
+
+    //获取配置信息，得到具体的微服务
+    @Autowired
+    private DiscoveryClient client;
 
     @RequestMapping("/student/add")
     @ResponseBody
@@ -33,5 +39,22 @@ public class StudentController {
     @ResponseBody
     public List<Student> queryAllStudnet(){
         return studentService.queryAllStudent();
+    }
+
+    //获取注册进来的微服务信息
+    @RequestMapping("/student/discovery")
+    @ResponseBody
+    public Object discovery(){
+        //获取微服务清单
+        List<String> services = client.getServices();
+        System.out.println(services);
+
+        //获取具体微服务,通过具体微服务id（application.name）
+        List<ServiceInstance> instances = client.getInstances("SPRINGCLOUD-PROVIDER-STUDENT");
+
+        for (ServiceInstance instance:instances)
+            System.out.println(instance);
+
+        return client;
     }
 }
